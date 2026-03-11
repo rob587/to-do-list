9dc32cd9e4c91b8a97b78c7389be9d19d64e767f
rob587 <robertocamm02@yahoo.com>
2026-03-11 11:52:49 +0100

    Add React Router and new components for task management
    - Updated package.json and package-lock.json to include react-router-dom.
    - Added new components: DefaultLayout, CompletedTask, DetailTask, and ElimTask.
    - Removed unused react.svg asset.

---
+++ src/components/CompletedTask.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompletedTask = () => {
+  return <div></div>;
+};
+
+export default CompletedTask;
---
+++ src/components/DetailTask.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DetailTask = () => {
+  return <div></div>;
+};
+
+export default DetailTask;
